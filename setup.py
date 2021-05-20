61977367870426c274e22022abd4a5dfa86dba00
micheleantonazzi <m7242872428>
2021-05-20 16:19:24 +0200

    setup.py: updated requirements

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     tests_require=test_deps,
     # Add here the package dependencies
     install_requires=[
-        'gibson>=0.4',
+        'gibson>=0.7',
         'PyYaml'
     ],
     entry_points={
