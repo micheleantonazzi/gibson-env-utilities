bbf0e3f74d19996be2e7b406293bd2a3a69587bf
micheleantonazzi <m7242872428>
2021-05-25 09:28:45 +0200

    gibson_assets_manager.py: used trimesh to load obj

--- tests/test_gibson_assets_manager.py
+++ tests/test_gibson_assets_manager.py
@@ -1,12 +1,7 @@
-import os
-
-from gibson.assets.assets_manager import AssetsManager, AssetsPathNotSetException
-
 from gibson_env_utilities.assets_manager.gibson_assets_manager import GibsonAssetsManager
 
 assets_manager = GibsonAssetsManager()
 
 
 def test_load_obj():
-    mesh = assets_manager.load_obj('space7')
-    assert len(sum(map(lambda material: material.vertices, mesh.materials.values()), [])) == 3789552
+    assets_manager.load_obj('space7')
