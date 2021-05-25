9a774bc1a8623d5d45c35dc4887d335c98125a73
micheleantonazzi <m7242872428>
2021-05-25 17:06:22 +0200

    setup.py: added gibson config file

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import subprocess
 # To use a consistent encoding
 from codecs import open as copen
 
+import yaml
 from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -39,6 +40,11 @@ test_deps = [
 extras = {
     'test': test_deps,
 }
+
+# Create the gibson config file
+config_parameters = {'model_id': '', 'is_discrete': False, 'random': {'random_initial_pose': False, 'random_init_z_range': [-0.1, 0.1], 'random_target_pose': False, 'random_init_x_range': [-0.1, 0.1], 'random_init_y_range': [-0.1, 0.1], 'random_init_rot_range': [-0.1, 0.1]}, 'ui_num': 3, 'envname': 'TurtlebotNavigateEnv', 'target_orn': [0, 0, 1.57], 'speed': {'timestep': 0.00417, 'frameskip': 10}, 'initial_pos': [0, 0, 0], 'verbose': False, 'fov': 1.57, 'display_ui': True, 'resolution': 512, 'semantic_source': 2, 'target_pos': [0, 0, 0], 'use_filler': True, 'semantic_color': 2, 'mode': 'gui', 'output': ['nonviz_sensor', 'rgb_filled', 'depth', 'semantics'], 'ui_components': ['RGB_FILLED', 'DEPTH', 'SEMANTICS'], 'initial_orn': [0, 0, 0], 'show_diagnostics': False}
+with open(os.path.join(os.path.dirname(__file__), 'gibson_env_utilities', 'data', 'gibson_config_file.yaml'), mode='w') as gibson_config_file:
+    yaml.dump(config_parameters, gibson_config_file, default_flow_style=False)
 
 setup(
     name='gibson-env-utilities',
