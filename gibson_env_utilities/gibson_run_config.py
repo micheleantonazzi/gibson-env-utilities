5a4d7959196938076593fcdb2c13ea2ec8dae53b
micheleantonazzi <m7242872428>
2021-05-25 18:27:33 +0200

    gibson_run_config.py: added new methods

--- gibson_env_utilities/gibson_run_config.py
+++ gibson_env_utilities/gibson_run_config.py
@@ -24,15 +24,37 @@ class GibsonConfigRun:
         self._gibson_config_parameters['initial_pos'] = env_data[GibsonEnvironmentsData.KEY_FLOORS][floor][GibsonEnvironmentsData.KEY_POSITION]
         self._gibson_config_parameters['initial_orn'] = env_data[GibsonEnvironmentsData.KEY_FLOORS][floor][GibsonEnvironmentsData.KEY_ORIENTATION]
 
+        # Remove semantics if the environment is not semantically annotated
+        if not env_data[GibsonEnvironmentsData.KEY_HAS_SEMANTICS]:
+            self.remove_semantics()
+        else:
+            if env_data[GibsonEnvironmentsData.KEY_DATASET] == 'stanford':
+                self._gibson_config_parameters['semantic_source'] = 1
+                self._gibson_config_parameters['semantic_color'] = 3
+            elif env_data[GibsonEnvironmentsData.KEY_DATASET] == 'matterport':
+                self._gibson_config_parameters['semantic_source'] = 2
+                self._gibson_config_parameters['semantic_color'] = 2
+
         return self
 
     def is_discrete(self, discrete: bool) -> 'GibsonConfigRun':
         """
-        Sets the discrete parameters. It mus be True if the simulator is used with the PLAY utility, otherwise it must be False
-        :param discrete:
-        :return:
+        Sets the discrete parameters. It must be True if the simulator is used with the PLAY utility, otherwise it must be False.
+        :param discrete: the paramater value
+        :return: GibsonConfigRun
         """
         self._gibson_config_parameters['is_discrete'] = discrete
+        return self
+
+    def remove_semantics(self) -> 'GibsonConfigRun':
+        """
+        If an environment is semantically annotated, the semantic data are automatically synthesized and shown.
+        This method tells Gibson not to produce semantic data.
+        :return:
+        """
+        self._gibson_config_parameters['output'] = ['nonviz_sensor', 'rgb_filled', 'depth']
+        self._gibson_config_parameters['ui_components'] = ['RGB_FILLED', 'DEPTH']
+        self._gibson_config_parameters['ui_num'] = 2
         return self
 
     def write_to_file(self) -> str:
