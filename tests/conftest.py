32ceb8e7b7d2507ce69c118930eba1db01e07a33
micheleantonazzi <m7242872428>
2021-05-26 17:14:13 +0200

    changed assets directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,5 +8,5 @@ def pytest_configure(config):
     try:
         AssetsManager().get_assets_path()
     except AssetsPathNotSetException:
-        AssetsManager().set_assets_path(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'gibson_env_utilities', 'data')).save_assets_information()
+        AssetsManager().set_assets_path(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'gibson_env_utilities', 'data', 'assets_test')).save_assets_information()
         download_assets_core()
