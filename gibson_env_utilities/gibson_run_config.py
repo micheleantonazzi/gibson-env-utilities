c2b98fa8be98986e1b98d33cfd61099b7dbbff4a
micheleantonazzi <m7242872428>
2021-05-26 15:21:04 +0200

    gibson_run_config.py: added methods to select the semantic rendering mode

--- gibson_env_utilities/gibson_run_config.py
+++ gibson_env_utilities/gibson_run_config.py
@@ -1,26 +1,42 @@
 import os
+from typing import Type
+
 import yaml
+from gibson.envs.env_modalities import BaseRobotEnv
+from termcolor import colored
 
 from gibson_env_utilities.gibson_environments_data import GibsonEnvironmentsData
 
 
+class EnvironmentNotSemanticallyAnnotatedException(Exception):
+    pass
+
+
 class GibsonConfigRun:
-    def __init__(self):
+    def __init__(self, simulation_env: Type[BaseRobotEnv], world_name: str, floor: int):
+        """
+        Creates new instance of GibsonConfigRun.
+        This methods automatically sets the starting position and orientation according to the values specified in the environments_data.yaml file.
+        If an environment is semantically annotated, these information are rendered and shown to the user.
+        To remove them, use the method 'remove_semantics'
+        :param world_name: the name of the environment
+        :type world_name: str
+        :param floor: the floor where to place the robot
+        :type floor: int
+        :param simulation_env: the simulation environment to use in Gibson
+        """
+
         self._gibson_environments_data = GibsonEnvironmentsData()
         with open(os.path.join(os.path.dirname(__file__), 'data', 'gibson_config_file.yaml'), mode='r') as config_file:
             self._gibson_config_parameters = yaml.load(config_file)
 
-    def set_environment_name_and_floor(self, environment_name: str, floor: int) -> 'GibsonConfigRun':
-        """
-        Sets the environment to load.
-        This methods automatically sets the starting position and orientation according to the values specified in the environments_data.yaml file.
-        :param environment_name: the name of the environment
-        :type environment_name: str
-        :param floor: the floor where to place the robot
-        :return: GibsonConfigClass
-        """
-        env_data = self._gibson_environments_data.get_environment_data(environment_name=environment_name)
-        self._gibson_config_parameters['model_id'] = environment_name
+        # Set the simulation environment
+        self._gibson_config_parameters['envname'] = simulation_env.__name__
+
+        self._environment_name = world_name
+
+        env_data = self._gibson_environments_data.get_environment_data(environment_name=world_name)
+        self._gibson_config_parameters['model_id'] = world_name
         self._gibson_config_parameters['initial_pos'] = env_data[GibsonEnvironmentsData.KEY_FLOORS][floor][GibsonEnvironmentsData.KEY_POSITION]
         self._gibson_config_parameters['initial_orn'] = env_data[GibsonEnvironmentsData.KEY_FLOORS][floor][GibsonEnvironmentsData.KEY_ORIENTATION]
 
@@ -35,8 +51,6 @@ class GibsonConfigRun:
                 self._gibson_config_parameters['semantic_source'] = 2
                 self._gibson_config_parameters['semantic_color'] = 2
 
-        return self
-
     def is_discrete(self, discrete: bool) -> 'GibsonConfigRun':
         """
         Sets the discrete parameters. It must be True if the simulator is used with the PLAY utility, otherwise it must be False.
@@ -50,11 +64,58 @@ class GibsonConfigRun:
         """
         If an environment is semantically annotated, the semantic data are automatically synthesized and shown.
         This method tells Gibson not to produce semantic data.
-        :return:
+        If there are no semantic data for the given environment, this method has no effect.
+        :return: GibsonConfigRun
         """
         self._gibson_config_parameters['output'] = ['nonviz_sensor', 'rgb_filled', 'depth']
         self._gibson_config_parameters['ui_components'] = ['RGB_FILLED', 'DEPTH']
         self._gibson_config_parameters['ui_num'] = 2
+        return self
+
+    def set_semantics_to_random_color(self) -> 'GibsonConfigRun':
+        """
+        This method sets the rendering of semantic information to 'Instance-by-Instance Color Coding' mode.
+        This modality consists in assigning a random distinctive color to each semantic instance.
+        These colors are arbitrarily chosen but are preserved through different trials.
+        Note that this mode renders intuitive colorful semantic map frames, but the rgb values do not enable easy semantic class lookup.
+        :raise EnvironmentNotSemanticallyAnnotatedException if the environment is not semantically annotated
+        :return: GibsonConfigRun
+        """
+        env_data = self._gibson_environments_data.get_environment_data(environment_name=self._environment_name)
+
+        if not env_data[GibsonEnvironmentsData.KEY_HAS_SEMANTICS]:
+            print(colored('The environment \'{0}\' is not semantically annotated, you cannot show semantic data!!'.format(self._environment_name), 'red'))
+            raise EnvironmentNotSemanticallyAnnotatedException()
+
+        self._gibson_config_parameters['semantic_color'] = 1
+
+        return self
+
+    def set_semantic_labels_to_color(self) -> 'GibsonConfigRun':
+        """
+        This method sets the rendering of semantic information to 'Semantic Label Color Coding' mode.
+        Using this semantic modality, the environment assigns a semantic label to each object category and each label is converted
+        to the corresponding RGB color using this formula:
+        b = ( label ) % 256;
+        g = ( label >> 8 ) % 256;
+        r = ( label >> 16 ) % 256;
+        color = {r, g, b}
+        Rendered colors usually look dark to human eyes, but rendered frames can be consumed directly
+        as semantically tagged pixel maps.
+        :raise EnvironmentNotSemanticallyAnnotatedException if the environment is not semantically annotated
+        :return: GibsonConfigRun
+        """
+        env_data = self._gibson_environments_data.get_environment_data(environment_name=self._environment_name)
+
+        if not env_data[GibsonEnvironmentsData.KEY_HAS_SEMANTICS]:
+            print(colored('The environment \'{0}\' is not semantically annotated, you cannot show semantic data!!'.format(self._environment_name), 'red'))
+            raise EnvironmentNotSemanticallyAnnotatedException()
+
+        if env_data[GibsonEnvironmentsData.KEY_DATASET] == 'stanford':
+            self._gibson_config_parameters['semantic_color'] = 3
+        elif env_data[GibsonEnvironmentsData.KEY_DATASET] == 'matterport':
+            self._gibson_config_parameters['semantic_color'] = 2
+
         return self
 
     def write_to_file(self) -> str:
