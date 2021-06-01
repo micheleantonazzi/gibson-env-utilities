bbcbee6e21f7b04b99d74e7c55efbc1220d29e73
micheleantonazzi <m7242872428>
2021-06-01 11:32:21 +0200

    create_floor_map_and_metadata.py: changed save_to_file False

--- examples/create_floor_map_and_metadata.py
+++ examples/create_floor_map_and_metadata.py
@@ -2,5 +2,5 @@ from matplotlib import pyplot as plt
 
 from gibson_env_utilities.gibson_assets_utilities import GibsonAssetsUtilities
 
-GibsonAssetsUtilities().create_floor_map(env_name='house1', floor=0, image_size='auto', save_to_file=True)
+GibsonAssetsUtilities().create_floor_map(env_name='house1', floor=0, image_size='auto', save_to_file=False)
 plt.show()
