80f9120423461b5ae783e5aa2c347d34421a5a68
micheleantonazzi <m7242872428>
2021-06-01 11:08:45 +0200

    added house2 map

--- examples/generate_voronoi_graph.py
+++ examples/generate_voronoi_graph.py
@@ -1,6 +1,6 @@
 import cv2
 from gibson_env_utilities.voronoi_graph_generator import VoronoiGraphGenerator
 
-voronoi_bitmap = VoronoiGraphGenerator(env_name='house1', floor=0).generate_voronoi_bitmap()
+voronoi_bitmap = VoronoiGraphGenerator(env_name='space7', floor=0).generate_voronoi_bitmap()
 cv2.imshow('voronoi bitmap', voronoi_bitmap)
 cv2.waitKey()
