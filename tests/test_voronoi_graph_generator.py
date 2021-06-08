cccb44c28f6a784af82258bc1425522a656e553c
micheleantonazzi <m7242872428>
2021-06-08 18:30:53 +0200

    upgraded test

--- tests/test_voronoi_graph_generator.py
+++ tests/test_voronoi_graph_generator.py
@@ -36,3 +36,18 @@ def test_graph_connected_components():
             components_image[node.get_coordinate().to_img_index()] = 0
 
     assert np.array_equal(components_image, voronoi_bitmap)
+
+
+def test_positions():
+    voronoi_graph_generator = VoronoiGraphGenerator(env_name='house1', floor=0)
+    voronoi_graph_generator.generate_voronoi_bitmap(save_to_file=False)
+    graph = voronoi_graph_generator.get_voronoi_graph()
+    graph_bitmap = graph.get_graph_bitmap()
+    positions = graph.get_real_position(0.01)
+
+    wrong_positions = 0
+    for position in positions:
+        if graph_bitmap[position.to_img_index()] != 0:
+            wrong_positions += 1
+
+    assert wrong_positions == 0
