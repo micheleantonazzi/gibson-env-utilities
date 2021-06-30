d95340563fa252f825a2fcc58c94540d7a521101
micheleantonazzi <m7242872428>
2021-06-30 17:58:12 +0200

    test_gibson_environment_data.py: fixed

--- tests/test_gibson_environment_data.py
+++ tests/test_gibson_environment_data.py
@@ -3,16 +3,16 @@ from gibson_env_utilities.gibson_environments_data import GibsonEnvironmentsData
 
 def test_get_data():
     data = GibsonEnvironmentsData()
-    assert len(data.get_environments_data().keys()) == 665
+    assert len(data.get_environments_data().keys()) == 666
 
     assert data.get_environments_data()['house1'] == data.get_environment_data('house1')
 
 
 def test_get_semantic_environments():
     data = GibsonEnvironmentsData()
-    assert len(data.get_environments_with_semantics()) == 88
+    assert len(data.get_environments_with_semantics()) == 89
 
 
 def test_get_environments_name():
     data = GibsonEnvironmentsData()
-    assert len(data.get_env_names()) == 665
+    assert len(data.get_env_names()) == 666
